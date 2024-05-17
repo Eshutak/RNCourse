320bf0de8c87e5d7b2f080b92ac0dc6236d6f9df
Eshutak <theaven0315t@gmail.com>
2024-05-17 11:22:12 +0900

    2024.05.17 lecture 17

--- App.js
+++ App.js
@@ -3,7 +3,14 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      {/*define style by inline */}
       <Text>this is first my app .</Text>
+      
+      {/*
+        define style by inline 
+        defining style is similar to CSS language. but not all same.
+      */} 
+      <Text style={{margin : 16, padding : 16, borderColor: "red", borderWidth : 1}}> another text.</Text>
       <Button title="tap me!"></Button>
     </View>
   );
@@ -16,4 +23,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText : {
+    margin : 16, 
+    padding : 16, 
+    borderColor: "red", 
+    borderWidth : 1
+  }
 });

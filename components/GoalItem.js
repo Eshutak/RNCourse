920d75c20968f31d27d72f5155752996104d343b
Eshutak <theaven0315t@gmail.com>
2024-05-24 13:06:59 +0900

    2024.05.24 lecture 2-33

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -6,24 +6,40 @@ function GoalItem(props){
     // method bind is javascript standard method 
     // it means that the method will be called later.
     // first parameter is 'this' that means 'onDeleteItem'.
-    return <Pressable onPress={props.onDeleteItem.bind(this, props.item)}>
-        <View key={props.item.id} style={styles.goalItem}>
-            <Text style={styles.goalText}> {props.item.text} </Text>
+
+    //android_ripple -> when component is touched component border's style is changed by difined style.
+    //in case iOS, use 'style' attribute of 'Pressable' conditionally.
+    //in the style definition, we can get pressed status information by 'pressed'.
+    return (<View key={props.item.id} style={styles.goalItem}>
+            <Pressable 
+                android_ripple={{color:'#210644'}}
+                onPress={props.onDeleteItem.bind(this, props.item)}
+                style={({pressed}) => pressed && styles.pressedItem}
+            >
+                <Text style={styles.goalText}> {props.item.text} </Text>
+            </Pressable>
         </View>
-    </Pressable>
-    ;
+    );
+    
+    
+    
 };
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
-goalItem : {
-    margin : 8,
-    padding : 8 ,
-    borderRadius : 6,
-    backgroundColor : '#5e0acc',
+    goalItem : {
+        margin : 8,
+        borderRadius : 6,
+        backgroundColor : '#5e0acc',
+
     },
+    pressedItem : {
+        opacity : 0.5,
+        backgroundColor:'#210644',
+    }, 
     goalText : {
-    color : 'white',
+        color : 'white',
+        padding : 8 ,
     },
 });
